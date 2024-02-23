5ace6793affce822cb3e8df9e33cb02b1c1e214b
Amit Garg <amitgarg0403@gmail.com>
2024-02-24 04:32:34 +0530

    Add mark to Favourite city

--- Backend/server.js
+++ Backend/server.js
@@ -19,16 +19,25 @@ app.get("/", (req,res)=>{
 
 app.get("/:cityname/:units", (req,res) =>{
     console.log(req.params);
-    let city = req.params.cityname
-    let unit;
-    if(req.params.units === "C")
-    {unit = "metric"}
-    else{unit = "imperial"}
+    let city = req.params.cityname;
+    let unit = req.params.units === "C" ? "metric" : "imperial";
     let url = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units="+ unit +"&appid="+ process.env.API_KEY
     console.log(url);
     axios.get(url)
     .then(response=>{
         res.send(response.data)
+    })
+    .catch(err=>{
+        console.error("Error fetching weather data:");
+            if (err.response && err.response.status === 404) {
+                // If the error response status is 404, send "City not found" message with 404 status code
+                console.log(1);
+                res.send({message: "City not found"});
+            } else {
+                // For other errors, send a generic error message with 500 status code
+                console.log(2);
+                res.status(500).send("Error fetching weather data. Please try again later.");
+            }
     })
 })
 
